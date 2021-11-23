cf956973edff4016acd3f2200302b56315c098c7
Jared Holgate <jared.holgate@hashicorp.com>
2021-11-23 12:48:55 +0000

    Create outputs.tf

---
+++ .github/workflows/outputs.tf
@@ -0,0 +1,11 @@
+output "login_server" {
+    value = azurerm_container_registry.maples.login_server
+}
+
+output "admin_username" {
+    value = azurerm_container_registry.maples.admin_username
+}
+
+output "admin_password" {
+    value = azurerm_container_registry.maples.admin_password
+}
